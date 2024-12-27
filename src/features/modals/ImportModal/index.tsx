ec4f9fe5260ea9a06c3abca588f08e0e89c93ebe
HALO\gangs <nprasad2077@gmail.com>
2024-12-27 04:40:31 -0600

    5mb change with graph support by increased nodes

--- src/features/modals/ImportModal/index.tsx
+++ src/features/modals/ImportModal/index.tsx
@@ -69,8 +69,8 @@ export const ImportModal = ({ opened, onClose }: ModalProps) => {
           <Dropzone
             onDrop={files => setFile(files[0])}
             onReject={files => toast.error(`Unable to load file ${files[0].file.name}`)}
-            maxSize={2 * 1024 * 1024}
-            maxFiles={1}
+            maxSize={5 * 1024 * 1024}
+            maxFiles={3}
             p="md"
             accept={[
               "application/json",
@@ -84,7 +84,7 @@ export const ImportModal = ({ opened, onClose }: ModalProps) => {
               <AiOutlineUpload size={48} />
               <Text fw="bold">Drop here or click to upload files</Text>
               <Text c="dimmed" fz="xs">
-                (Max 2 MB)
+                (Max 5 MB)
               </Text>
               <Text c="dimmed" fz="sm">
                 {file?.name ?? "None"}
